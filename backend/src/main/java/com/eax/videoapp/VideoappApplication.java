68e00e0f0aea677ea1a06ab3289e4c8661eec765
tencheeduard <164804241+Hmstrz@users.noreply.github.com>
2024-04-09 22:40:13 +0300

    start backend

---
+++ backend/src/main/java/com/eax/videoapp/VideoappApplication.java
@@ -0,0 +1,13 @@
+package com.eax.videoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eax/videoapp/VideoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eax.videoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
